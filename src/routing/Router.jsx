c804806bb49dd7dd7ff67fa257074653cdc7ec11
sebwierz <sebastianwierzba@web.de>
2023-01-03 13:22:22 +0100

    route update

--- src/routing/Router.jsx
+++ src/routing/Router.jsx
@@ -4,6 +4,7 @@ import Layout from '../components/Layout';
 export const router = createBrowserRouter(createRoutesFromElements(
 
     <Route path='/' element={<Layout/>}>
+        <Route></Route>
         
     </Route>
 ));
