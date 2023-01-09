6192ef86a91450f124a452616606095e3e22627e
sebwierz <sebastianwierzba@web.de>
2023-01-09 15:05:28 +0100

    carousel structure added

--- src/components/CarouselSection/index.jsx
+++ src/components/CarouselSection/index.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from '../../styles/CarouselSection.module.css'
 
 
 export default function CarouselSection(){
 
+    const data = ["1", "2", "3", "4"];
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const carouselInfiniteScroll = () => {
+        if (currentIndex === data.length-1){
+            return setCurrentIndex(0)
+        }
+        return setCurrentIndex(currentIndex+1)
+    }
+
+    useEffect(()=>{
+        const interval = setInterval(()=>{carouselInfiniteScroll()}, 3000)
+        //clean up
+        return() => clearInterval(interval);
+    })
+
     return(
         <section id="carousel" className={styles.carouselSection}>
-            <h1>Carousel</h1>
+            <div className={styles.carouselContainer}>
+               {data.map((item, index) => {
+                return <h1 className={styles.carouselItem} 
+                        style={{transform: `translate(-${currentIndex * 100}%)`}}
+                        key={index}>{item}</h1>})
+                }
+            </div>
         </section>
     )
 }
