e7fc134704bf75c760ed5d54f3c22adad0f1c8e2
sebwierz <sebastianwierzba@web.de>
2023-01-11 18:24:17 +0100

    restructuring aboutme-section, fixing responsiveness, reworking contact-section

---
+++ src/components/AboutMeSection/InnerSection/Image.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "../../../styles/MobileAboutMe.module.css"
+
+export default function Image() {
+
+    return(
+
+       <img src="../../../assets/sw.jpg" alt="Sebastian Wierzba"/>
+        
+    )
+}
